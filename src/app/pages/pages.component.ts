496b4ae984cfb5b850a72d6247498357dceafd86
Claudio A. Cuellar Valencia <claudio.cuellar@gmail.com>
2018-01-05 17:19:05 -0400

    Initial commit

---
+++ src/app/pages/pages.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { NbThemeService } from '@nebular/theme';
+import { StateService } from '../@core/data/state.service';
+
+import { MENU_ITEMS } from './pages-menu';
+
+@Component({
+  selector: 'ngx-pages',
+  template: `
+    <ngx-sample-layout>
+      <nb-menu [items]="menu"></nb-menu>
+      <router-outlet></router-outlet>
+    </ngx-sample-layout>
+  `,
+})
+export class PagesComponent {
+
+  menu = MENU_ITEMS;
+  sidebars = [];
+
+  constructor(protected stateService: StateService) {
+    this.stateService.getSidebarStates()
+      .subscribe((sidebars: any[]) => this.sidebars = sidebars);
+  }
+
+  ngOnInit() {
+    //Usando el sidebar en el lado derecho
+    this.stateService.setSidebarState(this.sidebars[1]);
+  }
+}
