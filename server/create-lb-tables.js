2b5b65cbe422695e4803dd8e1f359b4751c85b7f
primo <tsuki.spam13@gmail.com>
2018-01-10 11:56:34 -0400

    señales añadidas

--- server/create-lb-tables.js
+++ server/create-lb-tables.js
@@ -1,6 +1,6 @@
 var server = require('./server');
 var ds = server.dataSources.db;
-var lbTables = ['AccessToken', 'ACL', 'RoleMapping', 'Role','usuario'];
+var lbTables = ['AccessToken', 'ACL', 'RoleMapping', 'Role','usuario','signal'];
 ds.automigrate(lbTables, function(er) {
     if (er) throw er;
     console.log('Loopback tables [' - lbTables - '] created in ', ds.adapter.name);
