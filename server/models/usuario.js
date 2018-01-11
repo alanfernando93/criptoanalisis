9d0cf02dc778806514d3c577728b69142cd6a915
americo <americosombra@gmail.com>
2018-01-10 23:19:24 -0400

    tabla noticia
    noticia completa
    
    usuario exttendido
    
    noticias actualizado
    
    extension user
    
    pruebas de querying

--- server/models/usuario.js
+++ server/models/usuario.js
@@ -1,5 +1,18 @@
 'use strict';
 
 module.exports = function(Usuario) {
-
+    Usuario.getNoticias= function(cb){
+        Usuario.find({
+            where:{
+                id: $state.params.id
+            },
+            "include":{
+                "relation":"noticia"
+            }
+        })
+    }
+    Usuario.remoteMethod('noticia_usuario',{
+        returns: {arg: 'Usuarios', type: 'array'},
+        http: {path:'/noticias', verb: 'get'}
+    })
 };
