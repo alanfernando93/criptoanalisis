22fd5a2e4e8038fe4f70af8c19e4d1c4cdff073f
Claudio A. Cuellar Valencia <claudio.cuellar@gmail.com>
2018-01-12 17:11:06 -0400

    Code refactor

--- src/app/pages/news/list/list.component.ts
+++ src/app/pages/news/list/list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Http, Response } from '@angular/http';
 
 import { NoticiasService } from '../noticias.service';
@@ -8,12 +8,15 @@ import { NoticiasService } from '../noticias.service';
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.scss'],
 })
-export class ListComponent {
+export class ListComponent implements OnInit {
 
   news:any;
   table:String = "news";
 
   constructor(private http: Http, private noticiasService: NoticiasService) {
+  }
+
+  ngOnInit() {
     this.noticiasService.getNoticias().then((noticias) => {
       this.news = noticias;
     });
