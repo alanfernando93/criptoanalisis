63ea37c0ef98d3799fb9a707aeceb5ffe866ff7d
primo <tsuki.spam13@gmail.com>
2018-01-16 18:04:59 -0400

    conflictos arreglados

--- server/boot/create-build-in-models.js
+++ server/boot/create-build-in-models.js
@@ -1,7 +1,7 @@
 //TODO: Se ejecutara cada vez que se inicie la aplicacion, borrando todos los datos en estas tablas? MOVER?
 var server = require('../server');
 var ds = server.dataSources.db;
-var lbTables = ['AccessToken', 'ACL', 'RoleMapping', 'Role'];
+var lbTables = ['User','AccessToken', 'ACL', 'RoleMapping', 'Role'];
 
 ds.automigrate(lbTables, function(er) {
   if (er) throw er;
