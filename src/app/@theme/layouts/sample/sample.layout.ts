f7ae06cc652225283042795804cad406a9350df9
Eric A <LinkAckmann26@gmail.com>
2018-01-25 19:13:58 -0400

    cambios en el diseño de de header

--- src/app/@theme/layouts/sample/sample.layout.ts
+++ src/app/@theme/layouts/sample/sample.layout.ts
@@ -1,36 +1,77 @@
-import { Component, OnDestroy } from '@angular/core';
+import { Component, OnDestroy, OnInit } from "@angular/core";
 import {
   NbMediaBreakpoint,
   NbMediaBreakpointsService,
   NbMenuItem,
   NbMenuService,
   NbSidebarService,
-  NbThemeService,
-} from '@nebular/theme';
+  NbThemeService
+} from "@nebular/theme";
 
-import { StateService } from '../../../@core/data/state.service';
+import { StateService } from "../../../@core/data/state.service";
+import { UserService } from '../../../@core/data/users.service';
 
-import { Subscription } from 'rxjs/Subscription';
-import 'rxjs/add/operator/withLatestFrom';
-import 'rxjs/add/operator/delay';
+import { Subscription } from "rxjs/Subscription";
+import "rxjs/add/operator/withLatestFrom";
+import "rxjs/add/operator/delay";
 
-import { MENU_ITEMS } from '../../../pages/pages-menu';
+import { MENU_ITEMS } from "../../../pages/pages-menu";
 
 // TODO: move layouts into the framework
 @Component({
-  selector: 'ngx-sample-layout',
-  styleUrls: ['./sample.layout.scss'],
+  selector: "ngx-sample-layout",
+  styleUrls: ["./sample.layout.scss"],
   template: `
     <nb-layout [center]="layout.id === 'center-column'" windowMode>
       <nb-layout-header fixed>
         <ngx-header [position]="sidebar.id === 'right' ? 'normal': 'inverse'"></ngx-header>
       </nb-layout-header>
 
-
       <nb-sidebar class="menu-sidebar"
                    tag="menu-sidebar"
                    responsive
                    [right]="sidebar.id === 'left'">
+
+        <nb-sidebar-header class="d-block d-sm-none" *ngIf="user" >
+          <nb-user [menu]="userMenu" [name]="user?.username" [picture]="user?.picture" (menuClick)="logout()"></nb-user>
+            <nb-action>
+              <span class="badge badge-secondary">2 $CA</span>
+            </nb-action>
+            <nb-action>
+              <span class="badge badge-secondary">17 F</span>
+            </nb-action>
+            <nb-action>
+              <span class="badge badge-secondary">25 P</span>
+            </nb-action>
+            <nb-action>
+            <a class="icon-container badge badge-secondary" href="#">
+              Sign Up
+            </a>
+          </nb-action>
+          <nb-action>
+            <a class="icon-container badge badge-secondary" href="#">
+              Log In
+            </a>
+          </nb-action>
+            <!--
+          <a href="#" class="btn btn-hero-success main-btn d-block d-sm-block">
+            <i class="ion ion-social-github"></i> <span>Support Us</span>
+          </a>
+          -->
+        </nb-sidebar-header>        
+
+        <nb-sidebar-header *ngIf="!user" class="d-block d-sm-none">
+          <nb-action>
+            <a class="icon-container badge badge-secondary" href="#">
+              Sign Up
+            </a>
+          </nb-action>
+          <nb-action>
+            <a class="icon-container badge badge-secondary" href="#">
+              Log In
+            </a>
+          </nb-action>
+        </nb-sidebar-header>
         
         <!-- <ng-content select="nb-menu"></ng-content> -->
         <nb-menu [items]="menu"></nb-menu>
@@ -47,52 +88,54 @@ import { MENU_ITEMS } from '../../../pages/pages-menu';
 
      
     </nb-layout>
-  `,
+  `
 })
-export class SampleLayoutComponent implements OnDestroy {
-
+export class SampleLayoutComponent implements OnDestroy, OnInit {
   menu = MENU_ITEMS;
-  
+  id;
+  user;
+  token;
+
   subMenu: NbMenuItem[] = [
     {
-      title: 'PAGE LEVEL MENU',
-      group: true,
+      title: "PAGE LEVEL MENU",
+      group: true
     },
     {
-      title: 'Buttons',
-      icon: 'ion ion-android-radio-button-off',
-      link: '/pages/ui-features/buttons',
+      title: "Buttons",
+      icon: "ion ion-android-radio-button-off",
+      link: "/pages/ui-features/buttons"
     },
     {
-      title: 'Grid',
-      icon: 'ion ion-android-radio-button-off',
-      link: '/pages/ui-features/grid',
+      title: "Grid",
+      icon: "ion ion-android-radio-button-off",
+      link: "/pages/ui-features/grid"
     },
     {
-      title: 'Icons',
-      icon: 'ion ion-android-radio-button-off',
-      link: '/pages/ui-features/icons',
+      title: "Icons",
+      icon: "ion ion-android-radio-button-off",
+      link: "/pages/ui-features/icons"
     },
     {
-      title: 'Modals',
-      icon: 'ion ion-android-radio-button-off',
-      link: '/pages/ui-features/modals',
+      title: "Modals",
+      icon: "ion ion-android-radio-button-off",
+      link: "/pages/ui-features/modals"
     },
     {
-      title: 'Typography',
-      icon: 'ion ion-android-radio-button-off',
-      link: '/pages/ui-features/typography',
+      title: "Typography",
+      icon: "ion ion-android-radio-button-off",
+      link: "/pages/ui-features/typography"
     },
     {
-      title: 'Animated Searches',
-      icon: 'ion ion-android-radio-button-off',
-      link: '/pages/ui-features/search-fields',
+      title: "Animated Searches",
+      icon: "ion ion-android-radio-button-off",
+      link: "/pages/ui-features/search-fields"
     },
     {
-      title: 'Tabs',
-      icon: 'ion ion-android-radio-button-off',
-      link: '/pages/ui-features/tabs',
-    },
+      title: "Tabs",
+      icon: "ion ion-android-radio-button-off",
+      link: "/pages/ui-features/tabs"
+    }
   ];
   layout: any = {};
   sidebar: any = {};
@@ -101,34 +144,54 @@ export class SampleLayoutComponent implements OnDestroy {
   protected sidebarState$: Subscription;
   protected menuClick$: Subscription;
 
-  constructor(protected stateService: StateService,
+  constructor(
+    protected stateService: StateService,
     protected menuService: NbMenuService,
     protected themeService: NbThemeService,
     protected bpService: NbMediaBreakpointsService,
-    protected sidebarService: NbSidebarService) {
-    this.layoutState$ = this.stateService.onLayoutState()
-      .subscribe((layout: string) => this.layout = layout);
+    protected sidebarService: NbSidebarService,
+    protected userService: UserService
+  ) {
+    this.layoutState$ = this.stateService
+      .onLayoutState()
+      .subscribe((layout: string) => (this.layout = layout));
 
-    this.sidebarState$ = this.stateService.onSidebarState()
+    this.sidebarState$ = this.stateService
+      .onSidebarState()
       .subscribe((sidebar: string) => {
         this.sidebar = sidebar;
       });
 
-    const isBp = this.bpService.getByName('is');
-    this.menuClick$ = this.menuService.onItemSelect()
+    const isBp = this.bpService.getByName("is");
+    this.menuClick$ = this.menuService
+      .onItemSelect()
       .withLatestFrom(this.themeService.onMediaQueryChange())
       .delay(20)
-      .subscribe(([item, [bpFrom, bpTo]]: [any, [NbMediaBreakpoint, NbMediaBreakpoint]]) => {
-
-        if (bpTo.width <= isBp.width) {
-          this.sidebarService.collapse('menu-sidebar');
+      .subscribe(
+        ([item, [bpFrom, bpTo]]: [
+          any,
+          [NbMediaBreakpoint, NbMediaBreakpoint]
+        ]) => {
+          if (bpTo.width <= isBp.width) {
+            this.sidebarService.collapse("menu-sidebar");
+          }
         }
-      });
+      );
+  }
+
+  ngOnInit(){
+    this.token = localStorage.getItem("auth_app_token");
+    this.id = Number.parseInt(localStorage.getItem("userId"));
+    this.userService.getUser(this.id, this.token).then((usuario)=>{
+      this.user = JSON.parse(usuario['_body']);
+    });
   }
 
   ngOnDestroy() {
     this.layoutState$.unsubscribe();
     this.sidebarState$.unsubscribe();
     this.menuClick$.unsubscribe();
+    this.user = null;
   }
+  
 }
