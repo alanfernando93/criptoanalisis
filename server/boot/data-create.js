4ffe54105c390b0adab85cff599e46949ddc09ae
primo <tsuki.spam13@gmail.com>
2018-01-26 17:31:41 -0400

    fixed images

--- server/boot/data-create.js
+++ server/boot/data-create.js
@@ -1,12 +1,9 @@
 'use strict';
 var path = require('path');
-module.exports = function(app) {
-    var loopback = require('loopback');
-
+module.exports = function(app){
     var homedir = (process.platform === 'win32') ? process.env.HOMEPATH : process.env.HOME;
     var ruta = path.join(homedir, 'loop');
-    var ds = loopback.createDataSource({
-      name: 'FileStorage',
+    var ds = app.dataSource('FileStorage', {
       connector: require('loopback-component-storage'),
       provider: 'filesystem',
       root: ruta,
