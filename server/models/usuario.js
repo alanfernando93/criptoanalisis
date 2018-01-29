90daae5378cac8dd7affdf7c3e6dfa6bd3210128
americo <americosombra@gmail.com>
2018-01-29 18:32:46 -0400

    usuario validaciones

--- server/models/usuario.js
+++ server/models/usuario.js
@@ -2,6 +2,21 @@
 var loopback = require('loopback');
 var base64Img = require('base64-img');
 module.exports = function(Usuario) {
+  //validacion de campos de usuario
+  Usuario.validatesPresenceOf('nombre', 'apellido', 'fecha_nacimiento');
+  Usuario.validatesLengthOf('password', {min: 5, message:
+     {min: 'Password is too short'}});
+  Usuario.validatesLengthOf('username',
+   {min: 5, message: {min: 'username is too short'}});
+  Usuario.validatesLengthOf('nombre',
+   {min: 3, message: {min: 'nombre is too short'}});
+  Usuario.validatesLengthOf('apellido', +
+   {min: 3, message: {min: 'apellido is too short'}});
+  Usuario.validatesUniquenessOf('email', {message: 'email is not unique'});
+  Usuario.validatesFormatOf('nombre', {with: /[a-zA-Z\-'\s]+/});
+  Usuario.validatesFormatOf('apellido', {with: /[a-zA-Z\-'\s]+/});
+  Usuario.validatesFormatOf('username', {with: /^[a-zA-Z]\w*$/});
+
   Usuario.upload = function(req, res, cb) {
     var Container = Usuario.app.models.Container;
     var user = req.params.id;
@@ -20,14 +35,15 @@ module.exports = function(Usuario) {
       returns: {arg: 'status', type: 'string'},
     }
   );
-  Usuario.observe('loaded', function(context, next) {
-    if (context.data.id != undefined){
-      var cont = context.data.id;
-      var homedir = (process.platform === 'win32') ? process.env.HOMEPATH : process.env.HOME;
-      base64Img.base64(homedir + '/loop/' + cont + '/perfil.png', (err, data)=> {
-        context.data.profile = data;
-        next();
-      });
-    };
-  });
+ // Usuario.observe('loaded', function(context, next) {
+   // if (context.data.id != undefined){
+    //  var cont = context.data.id;
+    //  var homedir = (process.platform === 'win32') ? process.env.HOMEPATH : process.env.HOME;
+    //  base64Img.base64(homedir + '/loop/' + cont + '/perfil.png', (err, data)=> {
+    //    context.data.profile = data;
+   //     next();
+   //   });
+   // };
+   // next();
+  //});
 };
