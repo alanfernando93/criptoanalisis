b8bc60814bc3292a568511384bda2d43ed60016b
Eric A <LinkAckmann26@gmail.com>
2018-02-06 17:34:54 -0400

    cambio

---
+++ src/app/@theme/components/carusel/carusel.component.ts
@@ -0,0 +1,36 @@
+/*
+import { Component, OnInit } from '@angular/core';
+import { NgbCarousel } from '@ng-bootstrap/ng-bootstrap/carousel/carousel.js';
+
+@Component({
+    selector: 'ngx-carusel',
+    templateUrl: 'carusel.component.html',
+    styleUrls: ['carusel.component.scss']
+})
+
+export class NgxComponent extends NgbCarousel implements OnInit {
+    constructor() { 
+        super();
+    }
+
+    ngOnInit() { }
+}
+
+
+import {NgbCarouselConfig} from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+  selector: 'ngbd-carousel-config',
+  templateUrl: './carousel-config.html',
+  providers: [NgbCarouselConfig] // add NgbCarouselConfig to the component providers
+})
+export class NgbdCarouselConfig {
+  constructor(config: NgbCarouselConfig) {
+    // customize default values of carousels used by this component tree
+    config.interval = 10000;
+    config.wrap = false;
+    config.keyboard = false;
+  }
+}
+
+*/
