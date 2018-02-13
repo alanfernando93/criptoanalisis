e550960960751842f12c14413ce9c2e5ba636669
Eric A <LinkAckmann26@gmail.com>
2018-02-13 19:46:08 -0400

    mejoras en el diseño de headertwo

--- src/app/pages/coins/coins.module.ts
+++ src/app/pages/coins/coins.module.ts
@@ -3,16 +3,17 @@ import { NgModule } from '@angular/core';
 import { ThemeModule } from './../../@theme/theme.module';
 import { CoinsRoutingModule, routedComponents } from './coins-routing.module';
 
+//import { NoticiasService } from './noticias.service';
+
 @NgModule({
-    imports: [
-        ThemeModule,
-        CoinsRoutingModule,
-    ],
-    declarations: [
-        ...routedComponents,
-    ],
-    providers: []
+  imports: [
+    ThemeModule,
+    CoinsRoutingModule,
+  ],
+  declarations:[
+    ...routedComponents,
+  ],
+  providers:[]
 })
+
 export class CoinsModule {}
-
-
