ae5d217c1ac2a33b0cea371fe048a1a8aba658b5
Eric A <LinkAckmann26@gmail.com>
2018-03-08 10:38:09 -0400

    mejoras en el diseño de noticias

--- src/app/pages/news/list/list.component.ts
+++ src/app/pages/news/list/list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Http, Response } from '@angular/http';
 
 import { NewsService } from "../news.service";
