e21acd67457ac26c20234d4fecaea330b51f033d
primo <tsuki.spam13@gmail.com>
2018-03-14 15:36:38 -0400

    likes en señales añadidos

--- server/boot/create-positions-models.js
+++ server/boot/create-positions-models.js
@@ -5,9 +5,6 @@ module.exports = function(app) {
   app.dataSources.db.autoupdate('position', function(err) {
     if (err) throw err;
   });
-  app.dataSources.db.autoupdate('LikeSignal', function(err) {
-    if (err) throw err;
-  });
   app.dataSources.db.autoupdate('commentSignal', function(err) {
     if (err) throw err;
   });
