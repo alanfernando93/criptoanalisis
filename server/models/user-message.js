82777fad0f9e8ce8747687b9f291c615ef5c2a1e
primo <tsuki.spam13@gmail.com>
2018-03-19 20:13:01 -0400

    chat personal de paga añadido

--- server/models/user-message.js
+++ server/models/user-message.js
@@ -1,5 +1,26 @@
 'use strict';
 
 module.exports = function(Usermessage) {
-
+  Usermessage.PersonalMessage = function(pair, cb) {
+    Usermessage.find({where: {idProper: pair}}, cb);
+  };
+  Usermessage.remoteMethod('PersonalMessage', {
+    accepts: {arg: 'pair', type: 'string', required: true},
+    http: {path: '/PersonalMessage/:pair', verb: 'get'},
+    returns: {arg: 'messages', type: 'Object'},
+  });
+  Usermessage.afterRemote('PersonalMessage', function(ctx, messages, next) {
+    var x = [];
+    var message = ctx.result.messages;
+    message.forEach((element, index) => {
+      var con = Usermessage.app.models.usuario.findById(element.senderId)
+      .then(resp=>{
+        ctx.result.messages[index].username = resp.username;
+      });
+      x.push(con);
+    });
+    Promise.all(x).then(resp=>{
+      next();
+    });
+  });
 };
