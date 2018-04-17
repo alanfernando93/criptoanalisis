e4491aac20523d0ffb772f4a69eb829ebb50a4c3
americopito <americosombra@gmail.com>
2018-04-17 16:02:31 -0500

    changes

--- src/app/pages/advisories/list/list.component.ts
+++ src/app/pages/advisories/list/list.component.ts
@@ -3,6 +3,8 @@ import { Http, Response } from '@angular/http';
 
 import { AdvisoriesService } from '../advisories.service';
 import { Session } from '../../../@core/data/session';
+import { UserService } from "../../../@core/data/users.service";
+import { NbAuthJWTToken, NbAuthService } from "@nebular/auth";
 
 
 @Component({
@@ -14,16 +16,26 @@ export class ListComponent extends Session implements OnInit {
     advisories: any;
     advisoriesbyid: any;
     private userId = this.getUserId();
+    user: any = null;
 
     constructor(
+        private userService: UserService,
+        private authService: NbAuthService,
+
         
         private http: Http,
         private AdvisoriesService: AdvisoriesService,
 
     ) {
         super()
+        this.authService.onTokenChange().subscribe((token: NbAuthJWTToken) => {
+            if (token.getValue() && localStorage.length != 0) {
+              let id = localStorage.getItem("userId")
+              this.userService.getById(id).subscribe(resp=> this.user = resp)
+            }
+          });
     }
-
+    
     ngOnInit() {
         
         this.getAdvisories()
