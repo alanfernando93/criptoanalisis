dd4a6c73be7cb8a272fe9bb9567cffe8ea7f3ff6
alanfernando <alanfernando93.am@gmail.com>
2018-04-24 19:30:12 -0400

    modified editor tinymce

--- src/app/@theme/components/tiny-mce/tiny-mce.component.ts
+++ src/app/@theme/components/tiny-mce/tiny-mce.component.ts
@@ -11,14 +11,13 @@ import {
 import "tinymce";
 
 import "tinymce/themes/modern";
-import "tinymce/plugins/advlist";
 import "tinymce/plugins/code";
 import "tinymce/plugins/link";
-import "tinymce/plugins/lists";
 import "tinymce/plugins/image";
 import "tinymce/plugins/media";
 import "tinymce/plugins/table";
 import "tinymce/plugins/imagetools";
+import 'tinymce/plugins/contextmenu';
 
 import "../../../../assets/plugins/tradingview";
 
@@ -29,7 +28,9 @@ import { environment } from '../../../../environments/environment'
 
 @Component({
   selector: "ngx-tiny-mce",
-  template: `
+  template: `<textarea>
+  <p style="text-align: center; font-size: 15px;"><img title="TinyMCE Logo" src="//www.tinymce.com/images/glyph-tinymce@2x.png" alt="TinyMCE Logo" width="110" height="97" />
+  </p></textarea>
   `,
   providers:[NewsService]
 })
@@ -52,39 +53,37 @@ export class TinyMCEComponent implements OnDestroy, AfterViewInit {
   ngAfterViewInit() {
     tinymce.init({
       target: this.host.nativeElement,
-      // selector:"textarea",
+      selector: 'textarea',
       menubar: false,
-      plugins: ["link image tradingview code", "media table imagetools lists"],
-      toolbar:
-        "undo redo | formatselect | bold italic backcolor underline | alignleft aligncenter alignright alignjustify | blockquote | bullist numlist | link image media | tradingview | table",
-      skin_url: "assets/skins/lightgray",
+      plugins: ['link image tradingview code', 'media table imagetools contextmenu'],
+      toolbar: 'undo redo | formatselect | bold italic backcolor underline | alignleft aligncenter alignright alignjustify | blockquote | bullist numlist | link image media | tradingview | table',
+      skin_url: 'assets/skins/lightgray',
+      theme: 'modern',
       setup: editor => {
         this.editor = editor;
-        editor.on("init", cont => {
+        editor.on('init', cont => {
           if (this.content) cont.target.setContent(this.content);
         });
-        editor.on("keyup change", () => {
+        editor.on('keyup change', () => {
           const content = editor.getContent();
           this.onEditorKeyup.emit(content);
         });
-        // editor.shortcuts.add('ctrl+a', function() {console.log("ctrl+")});
       },
-      image_description:false,
+      image_description: false,
       image_title: true,
-      automatic_uploads: true,
-      file_picker_types: "image",
-      image_advtab: true,
+      // automatic_uploads: true,
+      file_picker_types: 'image',
       file_picker_callback: (cb, value, meta) => {
-        var input = document.createElement("input");
-        input.setAttribute("type", "file");
-        input.setAttribute("accept", "image/*");
+        let input = document.createElement('input');
+        input.setAttribute('type', 'file');
+        input.setAttribute('accept', 'image/*');
         input.onchange = event => {
-          let myFile = event["target"]["files"][0];
-          var reader = new FileReader();
+          let myFile = event['target']['files'][0];
+          let reader = new FileReader();
           reader.onload = () => {
-            var id = "blobid" + new Date().getTime();
+            var id = 'blobid' + new Date().getTime();
             var blobCache = tinymce.activeEditor.editorUpload.blobCache;
-            var base64 = reader.result.split(",")[1];
+            var base64 = reader.result.split(',')[1];
             var blobInfo = blobCache.create(id, myFile, base64);
             blobCache.add(blobInfo);
 
@@ -97,22 +96,18 @@ export class TinyMCEComponent implements OnDestroy, AfterViewInit {
         };
         input.click();
       },
-      content_css: [
-        "//fonts.googleapis.com/css?family=Lato:300,300i,400,400i",
-        "//www.tinymce.com/css/codepen.min.css"
-      ],
-      images_upload_handler: (blobInfo, success, failure) => {    
+      images_upload_handler: (blobInfo, success, failure) => {
         let body = new FormData();
         body.append('file', blobInfo.blob(), blobInfo.filename());
         this.newsService.fullUploadFileImage(body).subscribe(data=>{
-          success(this.baseUrl + "containers/galery/download/" + blobInfo.filename())
+          //success(this.baseUrl + "containers/galery/download/" + blobInfo.filename())
         })                 
       },
-      images_dataimg_filter: function(img) {
-        console.log(img)
-        return img.hasAttribute('internal-blob');
-      },
       height: this.height,
+      content_css: [
+        '//fonts.googleapis.com/css?family=Lato:300,300i,400,400i',
+        '//www.tinymce.com/css/codepen.min.css'
+      ]
     });
   }
 
