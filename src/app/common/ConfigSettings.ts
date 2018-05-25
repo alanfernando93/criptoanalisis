73e026aa647b4fafc4478eb547a2e70e4a2b4d99
alanfernando93 <alanfernando93.am@gmail.com>
2018-05-25 01:17:13 -0400

    fixed

--- src/app/common/ConfigSettings.ts
+++ src/app/common/ConfigSettings.ts
@@ -1,8 +1,6 @@
 export const _GLOBAL = {
-//   apiUrl: 'http://backend.criptoanalisis.org/api/',
-//   Url: 'http://191.101.228.157:8080',
-apiUrl: 'http://192.168.1.15:3000/api/',
-Url: 'http://192.168.1.15:3000',
+  apiUrl: 'http://backend.criptoanalisis.org/api/',
+  Url: 'http://191.101.228.157:8080',
 }
 
 export const configCrud = {
