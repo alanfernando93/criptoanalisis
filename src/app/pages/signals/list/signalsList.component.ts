99d10418b70313bcc931c46fbcaddd58393e07f3
Eric A <LinkAckmann26@gmail.com>
2018-05-29 18:29:13 -0400

    eliminacion del html y scss de noticias y señales

--- src/app/pages/signals/list/signalsList.component.ts
+++ src/app/pages/signals/list/signalsList.component.ts
@@ -6,8 +6,17 @@ import { UserService } from "../../../@core/data/users.service";
 
 @Component({
   selector: 'ngx-signalsList',
-  templateUrl: './signalsList.component.html',
-  styleUrls: ['./signalsList.component.scss'],
+  template: `<ngx-signalAll [listSignal]="signals" design="col-lg-4 col-md-4 col-sm-6"></ngx-signalAll>
+  <nb-card *ngIf="count > 6">
+      <nb-card-body>
+        <div class="row block-level-buttons">
+          <div class="col-md-12">
+            <button type="button" class="btn btn-outline-primary btn-block" (click)="Upload()">Ver mas</button>
+          </div>
+        </div>
+      </nb-card-body>
+    </nb-card>`,
+  styles: [``],
 })
 export class signalsListComponent implements OnInit {
 
