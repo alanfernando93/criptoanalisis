f0bfd91a4be5974e581d606c34b5105f5c668c76
primo <tsuki.spam13@gmail.com>
2018-06-20 16:37:10 -0400

    updated permissions

--- backend/server/boot/storage-create.js
+++ backend/server/boot/storage-create.js
@@ -2,12 +2,10 @@
 const HttpErrors = require('http-errors');
 var path = require('path');
 module.exports = function(app) {
-  var homedir = (process.platform === 'win32') ? process.env.HOMEPATH : process.env.HOME;
-  var ruta = path.join(homedir, 'loop');
   var ds = app.dataSource('FileStorage', {
     connector: require('loopback-component-storage'),
     provider: 'filesystem',
-    root: ruta,
+    root: './loop',
     allowedContentTypes: [
       'image/jpg',
       'image/jpeg',
