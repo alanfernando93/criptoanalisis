061197524d6a2f5dd93fbbef1c2fa515fc8871c8
Eric A <LinkAckmann26@gmail.com>
2018-06-19 22:25:27 -0400

    CRIP-45, show only 5 news in dashboard

--- frontend/src/app/pages/dashboard/dashboard.component.ts
+++ frontend/src/app/pages/dashboard/dashboard.component.ts
@@ -11,54 +11,64 @@ import { SignalsService } from '../signals/signals.service';
 export class DashboardComponent implements OnInit {
 
   news: any;
-  signals: any;
-  limit: number = 3;
+  // signals: any;
+  limit: number = 5;
   increment: number = 0;
 
   constructor(
     private newsService: NewsService,
-    private signalsService: SignalsService,
+    // private signalsService: SignalsService,
   ) {
 
   }
 
   ngOnInit() {
     this.getNews();
-    this.getSignals();
+    // this.getSignals();
   }
 
   getNews() {
-    this.newsService.getAll().subscribe(news => {
-      news ? this.news = news : {};
+    this.newsService.getAllLimit(this.limit, this.increment).subscribe(data => {
+      data ? this.news = data : {};
       this.news.forEach((element, index) => {
-        const newsId = this.news[index].id;
-        this.newsService.getUserByNews(newsId).subscribe(data => {this.news[index].contentUser = [];
-          this.news[index].contentUser.push(data);
-          this.news[index].contentUser[0].fama.sort(function (a, b) {
-            return a.valor < b.valor;
+          const newsId = this.news[index].id;
+          this.newsService.getUserByNews(newsId).subscribe(user => {
+            this.news[index].contentUser = [];
+            this.news[index].contentUser.push(user);
           });
-          this.news[index].contentUser[0].fama.firstTwo = [];
-          this.news[index].contentUser[0].fama.firstTwo = this.news[index].contentUser[0].fama.splice(0, 2);
-        });
-      });
+          });
     });
+
+      // this.news.forEach((element, index) => {
+      //   const newsId = this.news[index].id;
+      //   this.newsService.getUserByNews(newsId).subscribe(data => {
+      //     this.news[index].contentUser = [];
+      //     this.news[index].contentUser.push(data);
+      //     this.news[index].contentUser[0].fama.sort(function (a, b) {
+      //       return a.valor < b.valor;
+      //     });
+      //     this.news[index].contentUser[0].fama.firstTwo = [];
+      //     this.news[index].contentUser[0].fama.firstTwo = this.news[index].contentUser[0].fama.splice(0, 2);
+      //   });
+      // });
+    // });
   }
 
-  getSignals() {
-    this.signalsService.getAllLimit(this.limit, this.increment).subscribe(signalsAll => {
-      signalsAll ? this.signals = signalsAll : {};
-      this.signals.forEach((element, index) => {
-        const signalId = this.signals[index].id;
-        this.signalsService.getUserBySignal(signalId).subscribe(user => {
-          this.signals[index].contentUser = [];
-          this.signals[index].contentUser.push(user);
-          this.signals[index].contentUser[0].fama.sort(function (a, b) {
-            return a.valor < b.valor;
-          });
-          this.signals[index].contentUser[0].fama.firstTwo = [];
-          this.signals[index].contentUser[0].fama.firstTwo = this.signals[index].contentUser[0].fama.splice(0, 2);
-        });
-      });
-    })
-  }
+  // getSignals() {
+  //   this.signalsService.getAllLimit(this.limit, this.increment).subscribe(signalsAll => {
+  //     signalsAll ? this.signals = signalsAll : {};
+  //     this.signals.forEach((element, index) => {
+  //       const signalId = this.signals[index].id;
+  //       this.signalsService.getUserBySignal(signalId).subscribe(user => {
+  //         this.signals[index].contentUser = [];
+  //         this.signals[index].contentUser.push(user);
+  //         this.signals[index].contentUser[0].fama.sort(function (a, b) {
+  //           return a.valor < b.valor;
+  //         });
+  //         this.signals[index].contentUser[0].fama.firstTwo = [];
+  //         this.signals[index].contentUser[0].fama.firstTwo = this.signals[index].contentUser[0].fama.splice(0, 2);
+  //       });
+  //     });
+  //   })
+  // }
 }
