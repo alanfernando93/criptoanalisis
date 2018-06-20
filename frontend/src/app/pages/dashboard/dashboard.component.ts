cdaeb45f349ed8be651e2c0166b7e671cdcdc45a
Eric A <LinkAckmann26@gmail.com>
2018-06-19 22:32:40 -0400

    CRIP-45, fixed change of eslint

--- frontend/src/app/pages/dashboard/dashboard.component.ts
+++ frontend/src/app/pages/dashboard/dashboard.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
 import { NewsService } from '../news/news.service';
-import { SignalsService } from '../signals/signals.service';
 
 @Component({
   selector: 'ngx-dashboard',
@@ -17,58 +16,24 @@ export class DashboardComponent implements OnInit {
 
   constructor(
     private newsService: NewsService,
-    // private signalsService: SignalsService,
   ) {
 
   }
 
   ngOnInit() {
     this.getNews();
-    // this.getSignals();
   }
 
   getNews() {
     this.newsService.getAllLimit(this.limit, this.increment).subscribe(data => {
       data ? this.news = data : {};
       this.news.forEach((element, index) => {
-          const newsId = this.news[index].id;
-          this.newsService.getUserByNews(newsId).subscribe(user => {
-            this.news[index].contentUser = [];
-            this.news[index].contentUser.push(user);
-          });
-          });
+        const newsId = this.news[index].id;
+        this.newsService.getUserByNews(newsId).subscribe(user => {
+          this.news[index].contentUser = [];
+          this.news[index].contentUser.push(user);
+        });
+      });
     });
-
-      // this.news.forEach((element, index) => {
-      //   const newsId = this.news[index].id;
-      //   this.newsService.getUserByNews(newsId).subscribe(data => {
-      //     this.news[index].contentUser = [];
-      //     this.news[index].contentUser.push(data);
-      //     this.news[index].contentUser[0].fama.sort(function (a, b) {
-      //       return a.valor < b.valor;
-      //     });
-      //     this.news[index].contentUser[0].fama.firstTwo = [];
-      //     this.news[index].contentUser[0].fama.firstTwo = this.news[index].contentUser[0].fama.splice(0, 2);
-      //   });
-      // });
-    // });
   }
-
-  // getSignals() {
-  //   this.signalsService.getAllLimit(this.limit, this.increment).subscribe(signalsAll => {
-  //     signalsAll ? this.signals = signalsAll : {};
-  //     this.signals.forEach((element, index) => {
-  //       const signalId = this.signals[index].id;
-  //       this.signalsService.getUserBySignal(signalId).subscribe(user => {
-  //         this.signals[index].contentUser = [];
-  //         this.signals[index].contentUser.push(user);
-  //         this.signals[index].contentUser[0].fama.sort(function (a, b) {
-  //           return a.valor < b.valor;
-  //         });
-  //         this.signals[index].contentUser[0].fama.firstTwo = [];
-  //         this.signals[index].contentUser[0].fama.firstTwo = this.signals[index].contentUser[0].fama.splice(0, 2);
-  //       });
-  //     });
-  //   })
-  // }
 }
